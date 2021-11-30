4fd48b517e9b5e8dc72f13639db555c10730a308
DG <wwwk08@naver.com>
2021-11-30 18:27:27 +0900

    AI_Class-Jangdonggil

---
+++ jupyter_upload/deep/3d_graphic.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+# In[4]:
+
+
+#참고 자료, 다중 선형회귀 '예측 평면' 3D로 보기
+
+import statsmodels.api as statm
+import statsmodels.formula.api as statfa
+#from matplotlib.pyplot import figure
+
+X = [i[0:2] for i in data]
+y = [i[2] for i in data]
+
+X_1=statm.add_constant(X)
+results=statm.OLS(y,X_1).fit()
+
+hour_class=pd.DataFrame(X,columns=['study_hours','private_class'])
+hour_class['Score']=pd.Series(y)
+
+model = statfa.ols(formula='Score ~ study_hours + private_class', data=hour_class)
+
+results_formula = model.fit()
+
+a, b = np.meshgrid(np.linspace(hour_class.study_hours.min(),hour_class.study_hours.max(),100),
+                   np.linspace(hour_class.private_class.min(),hour_class.private_class.max(),100))
+
+X_ax = pd.DataFrame({'study_hours': a.ravel(), 'private_class': b.ravel()})
+fittedY=results_formula.predict(exog=X_ax)
+
+fig = plt.figure()
+graph = fig.add_subplot(111, projection='3d')
+
+graph.scatter(hour_class['study_hours'],hour_class['private_class'],hour_class['Score'],
+              c='blue',marker='o', alpha=1)
+graph.plot_surface(a,b,fittedY.values.reshape(a.shape),
+                   rstride=1, cstride=1, color='none', alpha=0.4)
+graph.set_xlabel('study hours')
+graph.set_ylabel('private class')
+graph.set_zlabel('Score')
+graph.dist = 11
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
